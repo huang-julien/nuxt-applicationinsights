096ced3b2860b723a99b656986771c6e8c9fe603
julien huang <julien.huang@outlook.fr>
2023-10-26 00:03:18 +0200

    fix: directly access to raw object within the client plugin

--- src/runtime/app/plugin.client.ts
+++ src/runtime/app/plugin.client.ts
@@ -1,12 +1,13 @@
 import { defineNuxtPlugin } from "nuxt/app";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
-import { useRuntimeConfig } from "#imports";
+import { useRuntimeConfig, toRaw } from "#imports";
 
 export default defineNuxtPlugin({
     name: 'nuxt-applicationinsights:client',
     setup() {
         const runtimeConfig = useRuntimeConfig()
-        const applicationInsights = new ApplicationInsights(runtimeConfig.public.applicationinsights)
+
+        const applicationInsights = new ApplicationInsights(toRaw(runtimeConfig.public.applicationinsights))
 
         return {
             provide: {
