c4b4305b4ec4617b456cd198abc06a2ff6f27b60
julien huang <julien.huang@outlook.fr>
2023-12-02 18:15:35 +0100

    fix: track $fetch

--- src/runtime/app/plugin.client.ts
+++ src/runtime/app/plugin.client.ts
@@ -2,14 +2,27 @@ import { defineNuxtPlugin } from "nuxt/app";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import type { Snippet } from "@microsoft/applicationinsights-web";
 import { useRuntimeConfig, toRaw } from "#imports";
-
+import { createFetch } from "ofetch"
+// @ts-expect-error virtual file
+import { baseURL } from "#build/paths.mjs"
+ 
 export default defineNuxtPlugin({
     name: 'nuxt-applicationinsights:client',
-    setup() {
+      setup(nuxtApp) {
         const runtimeConfig = useRuntimeConfig()
-
+ 
+        // @ts-expect-error
+        delete globalThis.$fetch
         const applicationInsights = new ApplicationInsights(toRaw(runtimeConfig.public.applicationinsights as Snippet))
+
         applicationInsights.loadAppInsights()
+        
+        globalThis.$fetch = createFetch({
+            defaults: {
+                baseURL: baseURL()
+            }
+        })
+
         return {
             provide: {
                 appInsights: applicationInsights
