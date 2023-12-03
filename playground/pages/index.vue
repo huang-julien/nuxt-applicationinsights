fc29aef5decad06a6ded330eaa9ed0eb2a19e15b
julien huang <julien.huang@outlook.fr>
2023-12-03 17:54:10 +0100

    chore: add playground

---
+++ playground/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+        <h1>Playground</h1>
+        <p>Playground</p>
+        <NuxtLink to="/second">
+            to second page
+        </NuxtLink>
+
+        <p>random number: {{ data }}</p>
+
+        <button @click="() => refresh()">refresh</button>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { useFetch } from '#imports'
+const { data, refresh } = await useFetch('/api/random-number')
+</script>
---
+++ playground/server/api/random-number.ts
@@ -0,0 +1,3 @@
+import { defineEventHandler } from "#imports"
+
+export default defineEventHandler(() => Math.floor(Math.random() * 100))
