2674c4db4c78ec649b7c910b9648f6b1af3abd75
Julien Huang <julien.huang@leetchi.com>
2024-02-22 09:34:24 +0100

    chore: add type anontation

--- src/runtime/app/plugin.server.ts
+++ src/runtime/app/plugin.server.ts
@@ -1,7 +1,18 @@
 import { defineNuxtPlugin } from "#imports"
 import { INITIAL_TRACE_KEY } from "./utils"
+import type NodeClient from 'applicationinsights/out/Library/NodeClient'
+import type Traceparent from 'applicationinsights/out/Library/Traceparent'
 
-export default defineNuxtPlugin({
+export default defineNuxtPlugin<{
+    appInsights: {
+        startTime: number;
+        client: NodeClient;
+        trace: Traceparent;
+        initialTrace: string;
+        properties: Record<string, string>;
+        shouldTrack: boolean;
+    }
+}>({
     name: 'nuxt-applicationinsights:server',
     enforce: "pre",
     setup(nuxtApp) {
