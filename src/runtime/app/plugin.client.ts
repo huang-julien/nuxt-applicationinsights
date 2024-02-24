42c7340a0a17261712290278666749cd79d17be8
julien huang <julien.huang@outlook.fr>
2024-02-24 22:49:43 +0100

    fix: set correct operation id, id and parent id for deps + page

--- src/runtime/app/plugin.client.ts
+++ src/runtime/app/plugin.client.ts
@@ -1,19 +1,19 @@
 import { defineNuxtPlugin } from "nuxt/app";
 import { ApplicationInsights, } from "@microsoft/applicationinsights-web";
 import type { Snippet } from "@microsoft/applicationinsights-web";
-import { useRuntimeConfig, toRaw, useRouter } from "#imports";
+import { useRuntimeConfig, toRaw, useRouter, useRoute } from "#imports";
 import { createFetch } from "ofetch"
 import { generateW3CId } from "@microsoft/applicationinsights-core-js"
 import { INITIAL_TRACE_KEY } from "./utils"
- // @ts-expect-error virtual file
+// @ts-expect-error virtual file
 import { baseURL } from "#build/paths.mjs"
 import { defu } from "defu"
- 
+
 export default defineNuxtPlugin({
     name: 'nuxt-applicationinsights:client',
     async setup(nuxtApp) {
         const runtimeConfig = useRuntimeConfig()
- 
+        const route = useRoute()
         const config: Snippet = {
             config: {}
         }
@@ -23,21 +23,25 @@ export default defineNuxtPlugin({
         // @ts-expect-error
         delete globalThis.$fetch
         const applicationInsights = new ApplicationInsights(defu(config, toRaw(runtimeConfig.public.applicationinsights as Snippet)))
-     
-        applicationInsights.loadAppInsights()
-        applicationInsights.addTelemetryInitializer((item) => {
-            if(!item.tags) { item.tags = [] }
 
-            if(item.baseType !== 'PageviewData') {
-                if(item.ext?.trace){
-                    item.ext.trace.traceID = generateW3CId()
-                    item.ext.trace.parentID = applicationInsights.context.telemetryTrace.traceID
-                }
-            }
-            item.tags['ai.operation.id'] = nuxtApp.payload.data[INITIAL_TRACE_KEY] 
+        applicationInsights.loadAppInsights()
+        applicationInsights.addDependencyListener(dep => {
+            dep.traceId = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[1]
         })
-        applicationInsights.context.telemetryTrace.parentID = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[1]
- 
+        applicationInsights.addTelemetryInitializer((item) => {
+            if (!item.tags) { item.tags = [] }
+
+            if (item.baseType === 'PageviewData' || item.baseType === 'PageviewPerformanceData') {
+                item.tags['ai.operation.parentId'] = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[2]
+                item.tags['ai.operation.id'] = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[1]
+            } else {
+                item.tags['ai.operation.parentId'] = applicationInsights.context.telemetryTrace.traceID
+            }
+            item.tags['ai.operation.id'] = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[1]
+        })
+        applicationInsights.context.telemetryTrace.traceID = generateW3CId()
+        applicationInsights.context.telemetryTrace.parentID = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[2]
+
         globalThis.$fetch = createFetch({
             defaults: {
                 baseURL: baseURL()
@@ -45,18 +49,16 @@ export default defineNuxtPlugin({
         })
 
         const router = useRouter()
-
-        router.beforeEach((to, from, next) => {  
-            applicationInsights.context.telemetryTrace.parentID = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[1]
-            applicationInsights.context.telemetryTrace.traceID = generateW3CId()
-            // TODO give users the choice between route name and route matched path
-            applicationInsights.startTrackPage(to.name as string)
-            next()
-        })
- 
-         router.afterEach((to) => {  
-            applicationInsights.stopTrackPage(to.name as string)
-            applicationInsights.flush()
+        // TODO give users the choice between route name and route matched path
+        applicationInsights.trackPageView({ name: route.name as string })
+        nuxtApp.hook('app:mounted', () => {
+            router.beforeResolve((to) => {
+                applicationInsights.context.telemetryTrace.parentID = nuxtApp.payload.data[INITIAL_TRACE_KEY].split('-')[2]
+                applicationInsights.context.telemetryTrace.traceID = generateW3CId()
+                // TODO give users the choice between route name and route matched path
+                applicationInsights.trackPageView({ name: to.name as string })
+                applicationInsights.flush()
+            })
         })
 
         return {
