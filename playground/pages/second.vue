333cb0bfa6d7a7f030ee8d9ec9a43fd130eeac88
Julien Huang <julien.h.dev@gmail.com>
2024-02-25 20:09:25 +0100

    fix: set correct operation id, id and parent id for deps + page (#29)

--- playground/pages/second.vue
+++ playground/pages/second.vue
@@ -4,5 +4,15 @@
     <NuxtLink to="/">
       to index page
     </NuxtLink>
+
+<p>random number: {{ data }}</p>
+
+<button @click="() => refresh()">
+  refresh
+</button>
   </div>
 </template>
+<script setup lang="ts">
+import { useFetch } from '#imports'
+const { data, refresh } = await useFetch('/api/random-number')
+</script>
