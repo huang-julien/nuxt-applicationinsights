6fc2d3bf51bf5df082940c44249a4ea3e51a8283
julien huang <julien.huang@outlook.fr>
2024-04-21 11:24:28 +0200

    lint

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,4 @@
 import { defineNuxtModule, createResolver, addServerPlugin, addTypeTemplate, addPlugin } from '@nuxt/kit'
-import { type TNitroAppInsightsConfig } from 'nitro-applicationinsights'
-import { defu } from 'defu'
-import type { Snippet } from '@microsoft/applicationinsights-web'
 import { resolvePath } from "mlly"
 
 export interface ApplicationInsightModuleOptions {
@@ -32,13 +29,9 @@ export default defineNuxtModule<ApplicationInsightModuleOptions>({
   },
   async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
-
-    nuxt.options.nitro.modules = nuxt.options.nitro.modules || []
-    nuxt.options.nitro.modules.push(await resolvePath('nitro-applicationinsights'))
-
     addTypeTemplate({
       filename: 'types/nuxt-applicationinsights.d.ts',
-       getContents() {
+      getContents() {
         return `/// <reference types="nitro-applicationinsights" />`
       }
     })
@@ -48,17 +41,20 @@ export default defineNuxtModule<ApplicationInsightModuleOptions>({
       references.push({ path: resolver.resolve('./runtime/types.d.ts') })
     })
 
-    if(options.serverEnabled) {
+    if (options.serverEnabled) {
+      nuxt.options.nitro.modules = nuxt.options.nitro.modules || []
+      nuxt.options.nitro.modules.push(await resolvePath('nitro-applicationinsights'))
+
       addPlugin({
         src: resolver.resolve('./runtime/app/plugin.server'),
         mode: 'server',
       })
-      
+
       // init config
       addServerPlugin(resolver.resolve('./runtime/server/plugins/setup'))
     }
 
-    if(options.clientEnabled) {
+    if (options.clientEnabled) {
       addPlugin({
         src: resolver.resolve('./runtime/app/plugin.client'),
         mode: 'client',
