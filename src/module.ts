28d091559d4e715a6043f155a34deab252da92df
Julien Huang <julien.h.dev@gmail.com>
2024-04-27 21:18:50 +0200

    feat: update nitro-applicationinsights to 0.8.0 (#49)

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,5 @@
 import { defineNuxtModule, createResolver, addServerPlugin, addTypeTemplate, addPlugin } from '@nuxt/kit'
-import { type TNitroAppInsightsConfig } from 'nitro-applicationinsights'
-import { defu } from 'defu'
-import type { Snippet } from '@microsoft/applicationinsights-web'
+import { resolvePath } from "mlly"
 
 export interface ApplicationInsightModuleOptions {
   /**
@@ -31,10 +29,9 @@ export default defineNuxtModule<ApplicationInsightModuleOptions>({
   },
   async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
-
     addTypeTemplate({
       filename: 'types/nuxt-applicationinsights.d.ts',
-       getContents() {
+      getContents() {
         return `/// <reference types="nitro-applicationinsights" />`
       }
     })
@@ -44,19 +41,20 @@ export default defineNuxtModule<ApplicationInsightModuleOptions>({
       references.push({ path: resolver.resolve('./runtime/types.d.ts') })
     })
 
-    if(options.serverEnabled) {
+    if (options.serverEnabled) {
+      nuxt.options.nitro.modules = nuxt.options.nitro.modules || []
+      nuxt.options.nitro.modules.push(await resolvePath('nitro-applicationinsights'))
+
       addPlugin({
         src: resolver.resolve('./runtime/app/plugin.server'),
         mode: 'server',
       })
-      
+
       // init config
       addServerPlugin(resolver.resolve('./runtime/server/plugins/setup'))
-      // awaiting for nitro module to transform nitro-applicationinsights into a nitro module
-      addServerPlugin(resolver.resolve('./runtime/server/plugins/applicationinsights'))
     }
 
-    if(options.clientEnabled) {
+    if (options.clientEnabled) {
       addPlugin({
         src: resolver.resolve('./runtime/app/plugin.client'),
         mode: 'client',
