65fcaf6cac088557c6a10a3ef47f53cde2c76efb
Julien Huang <julien.h.dev@gmail.com>
2024-06-07 23:46:10 +0200

    fix: access to raw runtime config

--- src/runtime/app/plugin.client.ts
+++ src/runtime/app/plugin.client.ts
@@ -4,13 +4,14 @@ import type { Snippet } from "@microsoft/applicationinsights-web";
 import { useRuntimeConfig } from "#imports";
 import { createFetch } from "ofetch"
 import { baseURL } from "#build/paths.mjs"
+import { toRaw } from "vue"
 
 export default defineNuxtPlugin({
     name: 'nuxt-applicationinsights:client',
     async setup(nuxtApp) {
         const runtimeConfig = useRuntimeConfig()
         const config: Snippet = {
-            config: runtimeConfig.public.applicationinsights ?? {}
+            config: toRaw(runtimeConfig.public).applicationinsights ?? {}
         }
 
         await nuxtApp.callHook('applicationinsights:config:client', config)
