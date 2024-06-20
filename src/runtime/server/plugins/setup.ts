b98ee4cd15bd0078022a26dd4dc4beeca5db928e
Julien Huang <julien.huang@leetchi.com>
2024-06-20 20:02:52 +0200

    fix: correct error telemetry

--- src/runtime/server/plugins/setup.ts
+++ src/runtime/server/plugins/setup.ts
@@ -1,6 +1,7 @@
 import { type NitroAppPlugin } from 'nitropack'
 import { useRuntimeConfig } from '#imports'
 import { defu } from 'defu'
+import { getQuery } from "h3"
 
 export default <NitroAppPlugin>((nitro) => {
     const runtimeConfig = useRuntimeConfig()
@@ -9,5 +10,21 @@ export default <NitroAppPlugin>((nitro) => {
         Object.assign(config, defu(runtimeConfig.applicationinsights, {
             connectionString: runtimeConfig.public.applicationinsights?.connectionString
         }))
+    })
+    
+    nitro.hooks.hook('applicationinsights:trackRequest:before', (event, telemetry) => {
+        if (event.path.startsWith('/__nuxt_error')) {
+			const queries = getQuery<{
+				url: string
+				statusCode: number
+				statusMessage: string
+			}>(event)
+
+			telemetry.url = queries.url
+			telemetry.success = queries.statusCode < 400
+			telemetry.resultCode = queries.statusCode
+			telemetry.name = `${event.method}: ${queries.url}`
+			telemetry.contextObjects!['ai.operation.name'] = `${event.method}: ${queries.url}`
+		}
     })
 })
