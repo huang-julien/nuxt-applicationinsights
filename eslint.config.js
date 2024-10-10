1faf07f0f7a5f3f1ec0dc43d452ce8bd87986dbe
Julien Huang <julien.h.dev@gmail.com>
2024-10-11 00:10:32 +0200

    fix: add back ignore files

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,9 @@ import withNuxt from "./.nuxt/eslint.config.mjs"
 export default withNuxt({
     rules: {
         'vue/multi-word-component-names': 'off'
-    }
+    },
+    ignores: [
+        'dist',
+        'node_modules'
+    ]
 })
