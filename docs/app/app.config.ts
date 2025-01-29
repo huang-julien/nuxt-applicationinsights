5f2679baaea84b02f65115ccc995db2e7e84f043
Julien Huang <julien.h.dev@gmail.com>
2025-01-29 21:12:45 +0100

    docs: add ui pro variables

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -7,6 +7,19 @@ export default defineAppConfig({
         left: 'text-sm text-gray-500 dark:text-gray-400',
         wrapper: 'border-t border-gray-200 dark:border-gray-800'
       }
+    },   
+    variables: {
+      light: {
+        background: '255 255 255',
+        foreground: 'var(--color-gray-700)'
+      },
+      dark: {
+        background: 'var(--color-gray-900)',
+        foreground: 'var(--color-gray-200)'
+      },
+      header: {
+        height: '4rem'
+      }
     }
   },
   seo: {
